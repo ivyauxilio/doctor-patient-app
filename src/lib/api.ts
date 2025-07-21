c1c038d10d348652c75e610b06725789c9828565
ivyauxilio <auxilioivy30@gmail.com>
2025-07-21 23:25:17 +0800

    update api url

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8000/api'; // Adjust as needed
-// const API_BASE_URL = 'https://staging.agilebeyond.net/api';
+// const API_BASE_URL = 'http://localhost:8000/api'; // Adjust as needed
+const API_BASE_URL = 'https://staging.agilebeyond.net/api';
 
 // Create Axios instance
 const api = axios.create({
@@ -52,8 +52,8 @@ export interface PatientData {
 }
 
 export const getCSRFToken = async () => {
-  await axios.get(`http://localhost:8000/sanctum/csrf-cookie`, {
-  // await axios.get(`https://staging.agilebeyond.net/sanctum/csrf-cookie`, {
+  // await axios.get(`http://localhost:8000/sanctum/csrf-cookie`, {
+  await axios.get(`https://staging.agilebeyond.net/sanctum/csrf-cookie`, {
     withCredentials: true,
   });
 };
